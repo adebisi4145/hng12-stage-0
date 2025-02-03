02d11e9084ecd8a2551793ec7cf62d8824bd3e5f
Adebisi Adeyemi <adeyemiadebisi00@yahoo.com>
2025-02-03 12:29:49 +0000

    first commit

---
+++ hng12-stage-0/Controllers/InformationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hng12_stage_0.Controllers
+{
+    public class InformationController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetInformation()
+        {
+            var response = new
+            {
+                email = "adeyemieyinjuoluwa@gmail.com",
+                current_datetime = DateTime.UtcNow.ToString("o"),
+                github_url = "https://github.com/adebisi4145/hng12-stage-0"
+            };
+
+            return Ok(response);
+        }
+    }
+}
